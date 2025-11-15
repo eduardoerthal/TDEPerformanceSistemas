8b7e227a1d7721cc1a536468cb960cfb8cfd09b5
eduardoerthal <eduardoerthaloliveira@gmail.com>
2025-11-15 14:16:38 -0300

    Trabalho feito

---
+++ Semaforo/src/main/java/org/example/Com.java
@@ -0,0 +1,43 @@
+package org.example;
+
+
+import java.util.concurrent.*;
+
+public class Com {
+    static int count = 0;
+    static final Semaphore sem = new Semaphore(1, true); // FIFO, justo
+
+    public static void main(String[] args) throws Exception {
+        int T = 8;
+        int I = 100000;
+        ExecutorService pool = Executors.newFixedThreadPool(T);
+        Runnable r = () -> {
+            for (int i = 0; i < I; i++) {
+                try {
+                    sem.acquire();
+                    count++;
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    sem.release();
+                }
+            }
+        };
+
+        long ti = System.currentTimeMillis();
+        for (int i = 0; i < I; i++) {
+            pool.submit(r);
+        }
+        pool.shutdown();
+        pool.awaitTermination(1, TimeUnit.MINUTES);
+
+        long tf = System.currentTimeMillis();
+        System.out.println("\nNumero Total de Threads: " + T);
+        System.out.println("\nNumero Total de Incrementos: " + I);
+        System.out.println("\nValor Esperado: " + T * I);
+        System.out.println("\nValor Obtido: " + count);
+        long tt = tf - ti;
+        System.out.println("\nTempo Total: " + tt + "ms!");
+    }
+}
+
---
+++ Semaforo/src/main/java/org/example/Sem.java
@@ -0,0 +1,33 @@
+package org.example;
+import java.util.concurrent.*;
+
+public class Sem {
+    static int count = 0;
+
+    public static void main(String[] args) throws Exception {
+        int T = 8;
+        int I = 100000;
+
+        ExecutorService pool = Executors.newFixedThreadPool(T);
+        Runnable r = () -> {
+            for (int i = 0; i < I; i++) {
+                count++;
+            }
+        };
+
+        long tInicio = System.currentTimeMillis();
+        for (int i = 0; i < T; i++){
+            pool.submit(r);
+        }
+        pool.shutdown();
+        pool.awaitTermination(1, TimeUnit.MINUTES);
+        long tFim = System.currentTimeMillis();
+        System.out.println("\nNumero Total de Threads: " + T);
+        System.out.println("\nNumero Total de Incrementos: " + I);
+        System.out.println("\nValor Esperado: " + T * I);
+        System.out.println("\nValor Obtido: " + count);
+        long tTot = tFim - tInicio;
+        System.out.println("\nTempo Total: " + tTot);
+
+    }
+}
